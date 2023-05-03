a47e80ca1459715ce5d540ee7fae48964b68a142
Faizan Ahmad <51469062+AndroidBiscuit@users.noreply.github.com>
2023-05-03 04:53:15 -0400

    Created basic react project with Vite and installed tailwindcss

---
+++ appointment-sheet-generator/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <button>
+          Test button
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Test project
+      </p>
+    </>
+  )
+}
+
+export default App
